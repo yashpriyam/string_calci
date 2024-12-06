c4211acda589c5112834e9acd7e5737346b56638
yashpriyam <yashpriyam74@gmail.com>
2024-12-06 16:37:42 +0530

    test newline as delimiter

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -8,19 +8,25 @@ describe('StringCalculator', () => {
         calculator = new StringCalculator();
     });
 
-    it('should return 0 if empty string is passed', () => {
+    it('1st: should return 0 if empty string is passed', () => {
         expect(calculator.add('')).toBe(0);
     });
 
-    it('should return the number itself if only one number is provided', () => {
+    it('2nd: should return the number itself if only one number is provided', () => {
         expect(calculator.add('1')).toBe(1);
     });
 
-    it('should return sum of two comma-separated numbers', () => {
+    it('3rd: should return sum of two comma-separated numbers', () => {
         expect(calculator.add('1,2')).toBe(3);
     });
 
-    it('should handle an unknown number of numbers', () => {
+    it('4th: should handle an unknown number of numbers', () => {
         expect(calculator.add('1,2,3,4,5')).toBe(15);
     });
+
+    it('5th: should handle newline as a delimiter along with commas', () => {
+        const calc = new StringCalculator();
+        expect(calc.add('1\n2,3')).toBe(6);
+    });
+    
 });
