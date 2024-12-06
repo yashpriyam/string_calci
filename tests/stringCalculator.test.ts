9ecfbd6e27b92a2c8754d9ade996c43edacb2a3c
yashpriyam <yashpriyam74@gmail.com>
2024-12-06 16:13:28 +0530

    add few failing tests

--- tests/stringCalculator.test.ts
+++ tests/stringCalculator.test.ts
@@ -8,7 +8,19 @@ describe('StringCalculator', () => {
         calculator = new StringCalculator();
     });
 
-    it('should return 0 if string is passed', () => {
+    it('should return 0 if empty string is passed', () => {
         expect(calculator.add('')).toBe(0);
+    });
+
+    it('should return the number itself if only one number is provided', () => {
+        expect(calculator.add('1')).toBe(1);
+    });
+
+    it('should return sum of two comma-separated numbers', () => {
+        expect(calculator.add('1,2')).toBe(3);
+    });
+
+    it('should handle an unknown number of numbers', () => {
+        expect(calculator.add('1,2,3,4,5')).toBe(15);
     });
 });
