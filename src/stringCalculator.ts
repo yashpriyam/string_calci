6ec2cff75b686add30ec23289c8fbb4097a09996
yashpriyam <yashpriyam74@gmail.com>
2024-12-06 16:36:34 +0530

    3rd and 4th test passes

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -3,6 +3,14 @@ export class StringCalculator {
         if (!input) {
             return 0
         }
+
+
+        if (input.includes(',')) {
+            const parts = input.split(',');
+            const nums = parts.map(n => parseInt(n, 10));
+            return nums.reduce((sum, val) => sum + val, 0);
+        }
+        
         return parseInt(input)
     }
 }
