d45147ff7bdd5c1e5da99f61cc5c1c4a5455cf2d
yashpriyam <yashpriyam74@gmail.com>
2024-12-06 16:34:01 +0530

    second test passes

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,6 +1,8 @@
 export class StringCalculator {
-    add(input: string): number {
-        const inputNumber = parseInt(input)
-        return isNaN(inputNumber) ? 0 : inputNumber
+    add(input: string): number | void {
+        if (!input) {
+            return 0
+        }
+        return parseInt(input)
     }
 }
