891089126ccdcecec67520f4d321b60b6d99a292
yashpriyam <yashpriyam74@gmail.com>
2024-12-06 16:52:38 +0530

    modify logic to pass 6th test

--- src/stringCalculator.ts
+++ src/stringCalculator.ts
@@ -1,13 +1,27 @@
 export class StringCalculator {
+    private static readonly DEFAULT_DELIMITERS = [',', '\n'];
+
     add(input: string): number | void {
         if (!input) {
             return 0
         }
 
-        input = input.replace(/\n/g, ',');
-        const parts = input.split(',');
+        const delimiters = [...StringCalculator.DEFAULT_DELIMITERS];
+        const customDelimiterMatch = input.match(/^\/\/(.+)\n(.*)$/);
+        if (customDelimiterMatch) {
+            const delimiterSection = customDelimiterMatch[1];
+            const numbersPart = customDelimiterMatch[2];
+            delimiters.push(delimiterSection);
+            input = numbersPart; 
+        }
+        const pattern = new RegExp(delimiters.map(d => this.escapeRegExp(d)).join('|'));
+
+        const parts = input.split(pattern).filter(x => x !== '');
         const nums = parts.map(n => parseInt(n, 10));
         return nums.reduce((sum, val) => sum + val, 0);
-        
+    }
+
+    private escapeRegExp(s: string): string {
+        return s.replace(/[.*+?^${}()|[\]\\]/g, '\\$&');
     }
 }
